0dca846cd25f98b6ee64157dfda897a3bb4eb9b6
md sajjat hossain <cgsaleh83@gmail.com>
2021-02-23 17:06:50 +0600

    ract todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // ============================= All state
+  const [addClass, setAddClass] = useState('item')
+  const [state, setState] = useState([]);
+
+  // ============================= AddItem Function
+  const addItem = () => {
+    if (state.length < 5) {
+      const item = { id: new Date().getTime().toString(), addClass };
+      setState([...state, item]);
+      setAddClass('item')
+    }else{
+      alert('Hei Your Item is already added In a 5 levels')
+    }
+  }
+
+  // ============================= ALL Item Remove Function
+  const removeItem = () => {
+    setState([]);
+    setAddClass('item')
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="box">
+      {/*  ============================= AddItem Button */}
+        <div>
+          <button onClick={() => addItem()}>Add Item</button>
+        </div>
+
+        <div>  {/*  ============================= Class Add Section */}
+          {
+            state.map((item, index) => {
+              return (
+                <div className="registration_form_data" key={item.id}>
+                  <div className={item.addClass}></div>
+                </div>
+              )
+            })
+          }
+        </div>
+        
+         {/*  ============================= Remove All Class */}
+        <div>
+          <button onClick={() => removeItem()}>Remove</button>
+        </div>
+      </div>
     </div>
   );
 }
